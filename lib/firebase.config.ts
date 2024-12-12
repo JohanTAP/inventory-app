175603b9161284cf0b810448274e0e9f706661fa
Johan Gutierrez <johgutierrez22@cftsa.cl>
2024-12-12 17:25:59 -0300

    Actualizar configuración de Firebase para usar variables de entorno

--- lib/firebase.config.ts
+++ lib/firebase.config.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAg7opyhm2X0iuE7Fcci17z3VU5JQRlbek",
-  authDomain: "inventory-adventista.firebaseapp.com",
-  databaseURL: "https://inventory-adventista-default-rtdb.firebaseio.com/",
-  projectId: "inventory-adventista",
-  storageBucket: "inventory-adventista.firebasestorage.app",
-  messagingSenderId: "868365654730",
-  appId: "868365654730:web:fc14771ae172ae5cdeb233",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
